01d5b83ef804604fee09c1a347601b69a2128a1a
Oksana19761219 <o.valioniene@gmail.com>
2022-06-27 21:17:55 +0300

    project created

---
+++ archeological_pottery_forms/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ArcheologicalPotteryFormsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'archeological_pottery_forms'
