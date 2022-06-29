0e9497c5c7298b3e741dc6a6ffdb3dc66b0e2303
Oksana19761219 <o.valioniene@gmail.com>
2022-06-29 21:03:44 +0300

    model 'Bibliography' modified

--- archeological_pottery_forms/models.py
+++ archeological_pottery_forms/models.py
@@ -5,22 +5,22 @@ from django.db import models
 class Bibliography(models.Model):
     author = models.CharField(
         'Author',
-        max_length=200,
+        max_length=250,
         help_text="enter the author's name"
     )
     title = models.CharField(
         'Title',
-        max_length=200,
+        max_length=700,
         help_text="enter the title of the report"
     )
     research_year = models.CharField(
         'Research Year',
-        max_length=20,
+        max_length=100,
         help_text="enter the year of archaeological research"
     )
     report_year = models.CharField(
         'Report Year',
-        max_length=20,
+        max_length=100,
         help_text="enter the year of the research report"
     )
     research_object_nr = models.IntegerField(
