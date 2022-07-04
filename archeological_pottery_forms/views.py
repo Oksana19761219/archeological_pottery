e93f69782b6e04539cc6e386dd35bc19efb42a73
Oksana19761219 <o.valioniene@gmail.com>
2022-07-04 20:44:56 +0300

    started working with templates

--- archeological_pottery_forms/views.py
+++ archeological_pottery_forms/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import \
+    Bibliography, \
+    BibliographicReference, \
+    PotteryLipShape, \
+    PotteryOrnamentShape, \
+    PotteryDescription
 
-def index(request):
-    return HttpResponse("Labas, pasauli!")
+def bibliography(request):
+    reports = Bibliography.objects.all()
+    reports_count = Bibliography.objects.all().count()
+    context = {
+        'reports': reports,
+        'reports_count': reports_count
+               }
+
+    return render(request, 'bibliography.html', context=context)
